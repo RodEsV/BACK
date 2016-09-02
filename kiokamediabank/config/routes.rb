33a0955b22813e73ff740dc10e1376f083c9e1e3
Théo Faure <ftheo@unal.edu.co>
2016-09-01 21:33:44 -0500

    devise installed not tested

--- kiokamediabank/config/routes.rb
+++ kiokamediabank/config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root'welcome#index'
+    get 'welcome/index'
+    root'welcome#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # standard devise routes available at /users
+    # NOTE: make sure this comes first!!!
+    devise_for :users
+
+    # token auth routes available at /api/v1/auth
+    namespace :api do
+      scope :v1 do
+        mount_devise_token_auth_for 'User', at: 'auth'
+      end
+    end
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
