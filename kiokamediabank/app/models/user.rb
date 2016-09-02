148f88e98c5ef650548f81f103adac56c0dc0e8d
Théo Faure <ftheo@unal.edu.co>
2016-09-02 00:03:54 -0500

    authentification works ;)

--- kiokamediabank/app/models/user.rb
+++ kiokamediabank/app/models/user.rb
@@ -2,6 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :omniauthable
+
   include DeviseTokenAuth::Concerns::User
+  before_save -> { skip_confirmation! }
+
+  def skip_confirmation!
+    self.confirmed_at = Time.now
+  end
+
 end
--- kiokamediabank/config/routes.rb
+++ kiokamediabank/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+    default_url_options :host => "localhost:3000"
+
     get 'welcome/index'
     root'welcome#index'
 
