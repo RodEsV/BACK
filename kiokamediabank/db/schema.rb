54ca3f61174f736f6654d6d2b9aac89dc5887605
dzalduar <dzalduar@unal.edu.co>
2016-09-13 14:33:36 -0500

    Added default value to uid in database

--- kiokamediabank/db/schema.rb
+++ kiokamediabank/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912031549) do
+ActiveRecord::Schema.define(version: 20160913192141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20160912031549) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               default: "email", null: false
-    t.string   "uid",                    default: "",      null: false
+    t.string   "uid",                    default: "email", null: false
     t.string   "encrypted_password",     default: "",      null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
