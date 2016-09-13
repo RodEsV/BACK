edf30436ed403d1aaf2f7112374786d564511819
Théo Faure <ftheo@unal.edu.co>
2016-09-12 22:36:29 -0500

    try to fix the fucking heroku bug by removing something in the api_controller

--- kiokamediabank/app/controllers/api/v1/api_controller.rb
+++ kiokamediabank/app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ApiController < ApplicationController
-  protect_from_forgery with :null_session, prepend :true
+  protect_from_forgery with :null_session#, prepend :true
   before_action :authenticate_user_from_token!, only:[:create, :update, :destroy]
 
   private
