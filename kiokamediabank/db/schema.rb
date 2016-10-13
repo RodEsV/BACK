0a88136890bde02fe0c5f55a91cc8180e828efff
dzalduar <dzalduar@unal.edu.co>
2016-10-13 16:55:28 -0500

    remove api controller comments to restablish device-token-auth

--- kiokamediabank/db/schema.rb
+++ kiokamediabank/db/schema.rb
@@ -33,41 +33,9 @@ ActiveRecord::Schema.define(version: 20161012195646) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "order_items", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "order_id"
-    t.decimal  "unit_price",  precision: 12, scale: 3
-    t.integer  "quantity"
-    t.decimal  "total_price", precision: 12, scale: 3
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
-    t.index ["order_id"], name: "index_order_items_on_order_id", using: :btree
-    t.index ["product_id"], name: "index_order_items_on_product_id", using: :btree
-  end
-
-  create_table "order_statuses", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.decimal  "subtotal",        precision: 12, scale: 3
-    t.decimal  "tax",             precision: 12, scale: 3
-    t.decimal  "shipping",        precision: 12, scale: 3
-    t.decimal  "total",           precision: 12, scale: 3
-    t.integer  "order_status_id"
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
-    t.index ["order_status_id"], name: "index_orders_on_order_status_id", using: :btree
-  end
-
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.string   "resolution"
     t.string   "image_link"
-    t.float    "price"
-    t.boolean  "active"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "type_id"
@@ -163,9 +131,6 @@ ActiveRecord::Schema.define(version: 20161012195646) do
     t.integer "quantity"
   end
 
-  add_foreign_key "order_items", "orders"
-  add_foreign_key "order_items", "products"
-  add_foreign_key "orders", "order_statuses"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "types"
 end
