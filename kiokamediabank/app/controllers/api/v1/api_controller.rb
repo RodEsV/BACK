c0c6eec125a88c1cda2b3f0065c8c6f7a394e63a
dzalduar <dzalduar@unal.edu.co>
2016-10-13 16:37:46 -0500

    remove api controller comments to restablish device-token-auth

--- kiokamediabank/app/controllers/api/v1/api_controller.rb
+++ kiokamediabank/app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ApiController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-   protect_from_forgery with :null_session, prepend: true
-   before_action :authenticate_user_from_token!, only:[:create, :update, :destroy]
+   #protect_from_forgery with :null_session, prepend: true
+   # => before_action :authenticate_user_from_token!, only:[:create, :update, :destroy]
 
    private
      def authenticate_user_from_token!
