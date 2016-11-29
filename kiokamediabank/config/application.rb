7d0746fc6018ad0a6271a442ca778e1babf2047d
Rodrigo Espinel <riespinelv@unal.edu.co>
2016-11-29 00:35:20 -0500

    headers exposed

--- kiokamediabank/config/application.rb
+++ kiokamediabank/config/application.rb
@@ -15,7 +15,10 @@ module Kiokamediabank
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete, :patch]
+        resource '*', 
+          :headers => :any, 
+          :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :methods => [:get, :post, :options, :put, :delete, :patch]
       end
     end
 
